0e4994515efec647e4cb8fe53cdc86672566bfcf
Marilyn Wren <Marilynwren64@yahoo.com>
2022-12-15 11:07:09 -0500

    commit tree 9738918df57242f4cb13d9e47f0f096f28bb0cb4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
+    
+    def self.create_with_title(title)
+        self.create(title: title)
+        
+    end 
+
+    def self.first_movie
+        self.first
+        
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        self.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        self.where('release_date > ?', 2002)
+    end
+
+    def self.update_with_attribute(attributes)
+        self.one.update(attributes:attributes)
+    end
+
+    def self.update_all_titles(title)
+        self.update(title: title)
+    end
+
+    def self.delete_by_id
+        self.destroy
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
+
 
 end
